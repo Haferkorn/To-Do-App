17eefeb25165c1a84681a0967ad701ff82d887ff
AnetteHaferkorn <anette.haferkorn@gmail.com>
2021-10-07 11:13:31 +0200

    Refactored to custom hook

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,42 +3,16 @@ import BoardsOverview from "./components/BoardsOverview";
 import NewTodo from "./components/NewTodo";
 import NavBar from "./components/NavBar";
 import styled from "styled-components/macro";
-import {useEffect, useState} from "react";
-import {deleteTodo, getTodos, postTodo, putTodo} from "./service/todo-api-service";
-import {getNextStatus} from "./service/todo-service";
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import BoardRender from "./components/BoardRender";
 import Details from "./components/Details";
+import useTodos from "./hooks/useTodos";
 
 
 
 function App() {
 
-    const [todos, setTodos] = useState([]);
-
-    const addTodo = (description) => {
-        postTodo(description)
-            .then(addedTodo => setTodos([...todos, addedTodo]))
-    }
-
-    const advanceTodo = (todo) => {
-        const newStatus = getNextStatus(todo.status)
-        const advancedTodo = {...todo, status: newStatus}
-        putTodo(advancedTodo)
-            .then(updatedTodo =>
-                setTodos(todos.map(item => updatedTodo.id === item.id ? advancedTodo : item)))
-    }
-
-    const removeTodo = (id) => {
-        deleteTodo(id)
-            .then(() => setTodos(todos.filter(todo => todo.id !== id)))
-    }
-
-
-    useEffect(() => {
-        getTodos()
-            .then(todos => setTodos(todos))
-    }, [])
+    const {todos, addTodo, advanceTodo, removeTodo}=useTodos()
 
     return (
         <Router>
